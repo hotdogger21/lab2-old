b0c3fd82e45ac17e9c890bc5c348f25b7b19961f
Poopbender21 <gustapes@student.gu.se>
2024-02-06 14:30:49 +0100

    add all files from lab1

--- CarController.java
+++ CarController.java
@@ -1,7 +1,10 @@
+import javax.naming.ldap.HasControls;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /*
 * This class represents the Controller part in the MVC pattern.
@@ -19,17 +22,25 @@ public class CarController {
     private Timer timer = new Timer(delay, new TimerListener());
 
     // The frame that represents this instance View of the MVC pattern
-    CarView frame;
+    protected CarView frame;
     // A list of cars, modify if needed
-    ArrayList<Car> cars = new ArrayList<>();
+    protected ArrayList<Car> cars = new ArrayList<>();
+
+
 
     //methods:
 
     public static void main(String[] args) {
+        Volvo240 car11 = new Volvo240();
+        car11.position.x = 100;
+        car11.position.y = 100;
         // Instance of this class
         CarController cc = new CarController();
 
-        // cc.cars.add(new Volvo240());
+
+        cc.cars.add(new Volvo240());
+        cc.cars.add(car11);
+
 
         // Start a new view and send a reference of self
         cc.frame = new CarView("CarSim 1.0", cc);
@@ -38,28 +49,44 @@ public class CarController {
         cc.timer.start();
     }
 
-    /* Each step the TimerListener moves all the cars in the list and tells the
-    * view to update its images. Change this method to your needs.
-    * */
-    private class TimerListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
- /*           for (ACar car : cars) {
-                car.move();
-                int x = (int) Math.round(car.getPosition().getX());
-                int y = (int) Math.round(car.getPosition().getY());
-                frame.drawPanel.moveit(x, y);
-                // repaint() calls the paintComponent method of the panel
-                frame.drawPanel.repaint();
-            }*/
+    public ArrayList<Car> getCars() {
+        return cars;
+    }
+
+    public void gas(int amount) {
+        double gas = ((double) amount) / 100;
+        for (Car car : cars) {
+            car.gas(gas);
         }
     }
 
-    // Calls the gas method for each car once
-    void gas(int amount) {
-        double gas = ((double) amount) / 100;
-       /* for (ACar car : cars
-                ) {
-            car.gas(gas);
-        }*/
+    public void brake(int amount) {
+        double brake = ((double) amount) / 100;
+        for (Car car : cars) {
+            car.brake(brake);
+        }
+    }
+
+
+    /* Each step the TimerListener moves all the cars in the list and tells the
+     * view to update its images. Change this method to your needs.
+     * */
+    private class TimerListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            for (Car car : cars) {
+                car.move();
+                int x = (int) Math.round(car.position.x);
+                int y = (int) Math.round(car.position.y);
+                int borderX = frame.drawPanel.getWidth();
+                int borderY = frame.drawPanel.getHeight();
+                frame.drawPanel.moveit(x, y);
+                // repaint() calls the paintComponent method of the panel
+                frame.drawPanel.repaint();
+                if(car.position.y + frame.drawPanel.volvoImage.getHeight() > borderY || car.position.y < 0){
+                    car.turnLeft();
+                    car.turnLeft();
+                }
+            }
+        }
     }
 }
--- DrawPanel.java
+++ DrawPanel.java
@@ -2,6 +2,8 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
@@ -10,24 +12,41 @@ import javax.swing.*;
 public class DrawPanel extends JPanel{
 
     // Just a single image, TODO: Generalize
+
+    CarController carC;
+
     BufferedImage volvoImage;
+    BufferedImage volvoWorkshopImage;
+    BufferedImage ScaniaImage;
+    BufferedImage SaabImage;
+
+
+
     // To keep track of a single car's position
     Point volvoPoint = new Point();
+    Point volvoPoint2 = new Point(100,100);
 
-    BufferedImage volvoWorkshopImage;
+    ArrayList<Point> pointlist = new ArrayList<>();
+
+    protected HashMap<Car, Point> ImagePoints = new HashMap<>();
+
     Point volvoWorkshopPoint = new Point(300,300);
 
     // TODO: Make this general for all cars
     void moveit(int x, int y){
-        volvoPoint.x = x;
-        volvoPoint.y = y;
+
+
     }
+
 
     // Initializes the panel and reads the images
     public DrawPanel(int x, int y) {
         this.setDoubleBuffered(true);
         this.setPreferredSize(new Dimension(x, y));
         this.setBackground(Color.green);
+
+
+
         // Print an error message in case file is not found with a try/catch block
         try {
             // You can remove the "pics" part if running outside of IntelliJ and
@@ -36,6 +55,8 @@ public class DrawPanel extends JPanel{
 
             // Rememember to rightclick src New -> Package -> name: pics -> MOVE *.jpg to pics.
             // if you are starting in IntelliJ.
+            ScaniaImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pics/Scania.jpg"));
+            SaabImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pics/Saab95.jpg"));
             volvoImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pics/Volvo240.jpg"));
             volvoWorkshopImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pics/VolvoBrand.jpg"));
         } catch (IOException ex)
@@ -50,7 +71,8 @@ public class DrawPanel extends JPanel{
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(volvoImage, volvoPoint.x, volvoPoint.y, null); // see javadoc for more info on the parameters
+        g.drawImage(SaabImage, volvoPoint.x, volvoPoint.y, null);
+        g.drawImage(volvoImage, volvoPoint2.x, volvoPoint2.y, null);
         g.drawImage(volvoWorkshopImage, volvoWorkshopPoint.x, volvoWorkshopPoint.y, null);
     }
 }
