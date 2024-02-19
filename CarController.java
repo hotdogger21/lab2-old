28686f8daf0d72fc1923cbcd48fb1b70c3f12ead
Poopbender21 <gustapes@student.gu.se>
2024-02-19 16:43:28 +0100

    Revert "CARS NOW CHANGE PCITURES WOW"
    This reverts commit 9d87b338bc06f37ecba231a90820d47c5930c11d.
    
    # Conflicts:
    #	CarController.java
    #	out/production/lab2-old/CarController$TimerListener.class
    #	out/production/lab2-old/CarController.class
    #	out/production/lab2-old/DrawPanel.class

--- CarController.java
+++ CarController.java
@@ -1,5 +1,11 @@
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.Console;
 import java.util.ArrayList;
+import java.util.HashMap;
+import static java.lang.System.out;
 
 /*
 * This class represents the Controller part in the MVC pattern.
@@ -11,16 +17,53 @@ public class CarController {
     // member fields:
 
     // The delay (ms) corresponds to 20 updates a sec (hz)
-
+    private final int delay = 50;
     // The timer is started with a listener (see below) that executes the statements
     // each step between delays.
+    private Timer timer = new Timer(delay, new TimerListener());
 
     // The frame that represents this instance View of the MVC pattern
-
+    protected CarView frame;
     // A list of cars, modify if needed
     protected ArrayList<Car> cars = new ArrayList<>();
 
-    //flytta dessa till carview kanske
+
+
+    //methods:
+
+    public static void main(String[] args) {
+        Volvo240 car1 = new Volvo240();
+        car1.position.x = 100;
+        car1.direction = 3;
+
+        Scania car2 = new Scania();
+        car2.position.x = 0;
+        car2.position.y = 100;
+
+        Saab95 car3 = new Saab95();
+        car3.position.x = 0;
+        car3.position.y = 200;
+
+        workshop<Volvo240> volvoworkshop = new workshop<>(5);
+        volvoworkshop.position.x = 300;
+        volvoworkshop.position.y = 300;
+
+
+        // Instance of this class
+        CarController cc = new CarController();
+
+        cc.cars.add(car1);
+        cc.cars.add(car2);
+        cc.cars.add(car3);
+
+
+        // Start a new view and send a reference of self
+        cc.frame = new CarView("CarSim 1.0", cc);
+
+        // Start the timer
+        cc.timer.start();
+    }
+
 
     public void gas(int amount) {
         double gas = ((double) amount) / 100;
@@ -80,4 +123,32 @@ public class CarController {
         }
     }
 
+
+    /* Each step the TimerListener moves all the cars in the list and tells the
+     * view to update its images. Change this method to your needs.
+     * */
+    private class TimerListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            for (Car car : cars) {
+                car.move();
+                int x = (int) Math.round(car.position.x);
+                int y = (int) Math.round(car.position.y);
+                int borderX = frame.drawPanel.getWidth();
+                int borderY = frame.drawPanel.getHeight();
+                frame.drawPanel.moveit(x, y, car);
+                // repaint() calls the paintComponent method of the panel
+                frame.drawPanel.repaint();
+                if(car.position.x + frame.drawPanel.volvoImage.getWidth() > borderX || car.position.x < 0){
+                    car.turnLeft();
+                    car.turnLeft();
+                }
+                if(car.position.y + frame.drawPanel.volvoImage.getHeight() > borderY || car.position.y < 0){
+                    car.turnLeft();
+                    car.turnLeft();
+                }
+
+
+            }
+        }
+    }
 }
